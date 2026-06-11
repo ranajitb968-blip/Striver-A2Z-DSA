d69230e51f64146171556694468fd850a85bc8cb
ranajitb968-blip <ranajitb968@gmail.com>
2026-06-12 00:04:21 +0530

    Added first C++ basic program

---
+++ firstclass_basicc++/basic1.cpp
@@ -0,0 +1,21 @@
+/*
+    This is the first program of C++ language. In this program we will print some text on the console.
+    This is a very basic program and it is used to understand the basic structure of a C++ program.
+    The main function is the entry point of the program and it is where the execution of the program starts.
+    The std::cout is used to print the text on the console and std::endl is used to move the cursor to the next line.
+
+#include<iostream>
+int main(){
+    std::cout<<"hi ranajit"<<std::endl;
+    std::cout<<"hi dsa you are the best friend of mind"<<std::endl;
+    return 0;
+}
+*/
+#include<iostream>
+using namespace std;
+int main(){
+    int x;
+    cin>>x;
+    cout<<"value of x is: "<<x;
+    return 0;
+}
