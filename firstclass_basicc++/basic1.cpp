c5ad75e7f1114b7093f487a7a6a431aecc48c291
ranajitb968-blip <ranajitb968@gmail.com>
2026-06-12 11:11:33 +0530

    solve new porblem in basic1.cpp

--- firstclass_basicc++/basic1.cpp
+++ firstclass_basicc++/basic1.cpp
@@ -10,7 +10,10 @@ int main(){
     std::cout<<"hi dsa you are the best friend of mind"<<std::endl;
     return 0;
 }
-*/
+    In this program we are taking input from the user and then printing the value of x on the console.
+    The std::cin is used to take input from the user and it is used to read the value of x from the console.
+    The std::cout is used to print the value of x on the console.
+
 #include<iostream>
 using namespace std;
 int main(){
@@ -19,3 +22,20 @@ int main(){
     cout<<"value of x is: "<<x;
     return 0;
 }
+*/
+#include<iostream>
+using namespace std;
+int main(){
+    int x;
+    int y;
+
+    cout<<"enter the value of x: ";
+    cin>>x;
+    cout<<"enter the value of y:";
+    cin>>y;
+
+    cout<<"value sum is :"<<x+y;
+
+    return 0;
+}
+
