65d1c0419f93c257f631d8916a34179ab06ed0df
ranajitb968-blip <ranajitb968@gmail.com>
2026-06-24 20:52:25 +0530

    basic1problem solve

--- firstclass_basicc++/basic1.cpp
+++ firstclass_basicc++/basic1.cpp
@@ -372,7 +372,7 @@ In the first test case, Nikita can put 1
 
 In the second test case, Nikita can only end up with either a tower with no blocks or a tower with 2
  blocks, so the answer is "No".
-*/
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -402,3 +402,91 @@ int main()
 
     return 0;
 }
+
+
+q)Nikita and LCM
+time limit per test2 seconds
+memory limit per test256 megabytes
+Nikita is a student passionate about number theory and algorithms. He faces an interesting problem related to an array of numbers.
+
+Suppose Nikita has an array of integers a
+ of length n
+. He will call a subsequence†
+ of the array special if its least common multiple (LCM) is not contained in a
+. The LCM of an empty subsequence is equal to 0
+.
+
+Nikita wonders: what is the length of the longest special subsequence of a
+? Help him answer this question!
+
+†
+ A sequence b
+ is a subsequence of a
+ if b
+ can be obtained from a
+ by the deletion of several (possibly, zero or all) elements, without changing the order of the remaining elements. For example, [5,2,3]
+ is a subsequence of [1,5,7,8,2,4,3]
+.
+
+Input
+Each test contains multiple test cases. The first line of input contains a single integer t
+ (1≤t≤2000
+) — the number of test cases. The description of the test cases follows.
+
+The first line of each test case contains a single integer n
+ (1≤n≤2000
+) — the length of the array a
+.
+
+The second line of each test case contains n
+ integers a1,a2,…,an
+ (1≤ai≤109
+) — the elements of the array a
+.
+
+It is guaranteed that the sum of n
+ over all test cases does not exceed 2000
+.
+
+Output
+For each test case, output a single integer — the length of the longest special subsequence of a
+.
+
+Example
+InputCopy
+6
+5
+1 2 4 8 16
+6
+3 2 10 20 60 1
+7
+2 3 4 6 12 100003 1200036
+9
+2 42 7 3 6 7 7 1 6
+8
+4 99 57 179 10203 2 11 40812
+1
+1
+OutputCopy
+0
+4
+4
+5
+8
+0
+Note
+In the first test case, the LCM of any non-empty subsequence is contained in a
+, so the answer is 0
+.
+
+In the second test case, we can take the subsequence [3,2,10,1]
+, its LCM is equal to 30
+, which is not contained in a
+.
+
+In the third test case, we can take the subsequence [2,3,6,100003]
+, its LCM is equal to 600018
+, which is not contained in a
+.
+
+*/
---
+++ firstclass_basicc++/basic1p.cpp
@@ -0,0 +1,19 @@
+
+/*this is problem statement are basic or operators problem 
+q) write a program to print your name,date of birth and mobile number 
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int phone_number;
+    string name;
+    int dob;
+
+    cout<<"my phone_number is : 9609794466"<<endl;
+    cout<<"my name is : Ranajit bera"<<endl;
+    cout<<"my date of birth is : 29/09/2005"<<endl;
+    return 0;
+}
+    
+q) write a progrma to enter length and breadth of a rectangle and finish its perimeter
+
