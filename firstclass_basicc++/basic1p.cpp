b60fe11d85f951982ca86094cf745e22927f8de3
ranajitb968-blip <ranajitb968@gmail.com>
2026-06-24 21:01:04 +0530

    new problem solve

--- firstclass_basicc++/basic1p.cpp
+++ firstclass_basicc++/basic1p.cpp
@@ -1,6 +1,6 @@
 
-/*this is problem statement are basic or operators problem 
-q) write a program to print your name,date of birth and mobile number 
+/*this is problem statement are basic or operators problem
+q) write a program to print your name,date of birth and mobile number
 
 #include<bits/stdc++.h>
 using namespace std;
@@ -14,6 +14,20 @@ int main(){
     cout<<"my date of birth is : 29/09/2005"<<endl;
     return 0;
 }
-    
-q) write a progrma to enter length and breadth of a rectangle and finish its perimeter
 
+q) write a progrma to enter length and breadth of a rectangle and finish its perimeter
+firstly you must be know that rectangle perimeter law 𝑃=2×(𝑙+𝑤)
+*/
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int l;
+    int p;
+    int w;
+
+    cin>>l;
+    cin>>w;
+    p = 2*(l+w);
+
+    cout<<"A rectangle perimeter is:" <<p <<endl; 
+}
