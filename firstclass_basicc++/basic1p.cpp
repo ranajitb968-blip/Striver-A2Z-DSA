f688c4592d6c5b067d301bc8144d264548cea2b6
ranajitb968-blip <ranajitb968@gmail.com>
2026-06-29 22:32:44 +0530

    switch case use that

--- firstclass_basicc++/basic1p.cpp
+++ firstclass_basicc++/basic1p.cpp
@@ -194,8 +194,8 @@ int main(){
 
     return 0;
 }
+q) function use how to use 
 
-*/
 #include<bits/stdc++.h>
 using namespace std;
 void mininum(int a, int b)
@@ -215,6 +215,49 @@ int main()
     cout << "enter two number: ";
     cin >> a >> b;
     mininum(7, 5);
+
+    return 0;
+}
+
+44. Write a program to print the day of the week name using a switch case.
+
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int day;
+    cout << "Enter the day number (1-7): ";
+    cin >> day;
+
+    switch (day)
+    {
+    case 1:
+        cout << "Monday" << endl;
+        break;
+    case 2:
+        cout << "Tuesday" << endl;
+        break;
+    case 3:
+        cout << "Wednesday" << endl;
+        break;
+    case 4:
+        cout << "Thursday" << endl;
+        break;
+    case 5:
+        cout << "Friday" << endl;
+        break;
+    case 6:
+        cout << "Saturday" << endl;
+        break;
+    case 7:
+        cout << "Sunday" << endl;
+        break;
+    default:
+        cout << "Invalid day number" << endl;
+    }
 
     return 0;
 }
