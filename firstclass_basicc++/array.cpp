ec2f4d4529ec210a6b73558d51fafa26ea89968a
ranajitb968-blip <ranajitb968@gmail.com>
2026-07-25 08:01:50 +0530

    new problem

--- firstclass_basicc++/array.cpp
+++ firstclass_basicc++/array.cpp
@@ -1,3 +1,4 @@
+/*
 #include<bits/stdc++.h>
 using namespace std;
 int main()
@@ -8,6 +9,29 @@ int main()
     cout<<"arr print of name "<<name[i]<<endl;
    
     }
+
+    return 0;
+}
+
+*/
+//user input from array
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int marks[5];
+
+    //take input
+    for(int i=0;i<=4;i++){
+        cout<<"enter the marks of student :"<<i+1<<endl;;
+        cin>>marks[i];
+    }
+
+    //output given
+    for(int i=0;i<=4;i++){
+        cout<<"marks of student is : "<<marks[i]<<endl;
+    }
+
 
     return 0;
 }
