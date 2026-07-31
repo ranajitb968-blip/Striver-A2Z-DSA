9c6ed146d923b977dc7f8ed92f9f1c0108990d63
ranajitb968-blip <ranajitb968@gmail.com>
2026-07-31 21:33:31 +0530

    new celsius problem

---
+++ firstclass_basicc++/basicclass/basic1.cpp
@@ -0,0 +1,252 @@
+/*
+    This is the first program of C++ language. In this program we will print some text on the console.
+    This is a very basic program and it is used to understand the basic structure of a C++ program.
+    The main function is the entry point of the program and it is where the execution of the program starts.
+    The std::cout is used to print the text on the console and std::endl is used to move the cursor to the next line.
+
+#include<iostream>
+int main(){
+    std::cout<<"hi ranajit"<<std::endl;
+    std::cout<<"hi ranajit"<<"\n";                //\n is used to move the cursor to the next line.
+    std::cout<<"hi dsa you are the best friend of mind"<<std::endl;
+    return 0;
+}
+    In this program we are taking input from the user and then printing the value of x on the console.
+    The std::cin is used to take input from the user and it is used to read the value of x from the console.
+    The std::cout is used to print the value of x on the console.
+
+#include<iostream>
+using namespace std;
+int main(){
+    int x;
+    cin>>x;
+    cout<<"value of x is: "<<x;
+    return 0;
+}
+
+    *In this program we are taking input from the user and then printing the sum of x and y on the console.
+    The std::cin is used to take input from the user and it is used to read the value of x and y from the console.
+    The std::cout is used to print the sum of x and y on the console.
+
+#include<bits/stdc++.h>  //that means this is a header file that includes all the standard libraries of C++.
+using namespace std;
+int main(){
+    int x;
+    int y;
+
+    cout<<"enter the value of x: ";
+    cin>>x;
+    cout<<"enter the value of y:";
+    cin>>y;
+
+    cout<<"value sum is :"<<x+y;
+
+    return 0;
+}
+
+
+q) Write a program to print your name, date of birth and mobile number.
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout<<"my name is : ranajit bera "<<"\n";
+    cout<<"my date of birth is : 20/12/2000 "<<"\n";
+    cout<<"my mobile number is : 7003030303 "<<"\n";
+}
+q) write a program to print you are eligible for voting or not based on your age.
+
+#include<iostream>
+using namespace std;
+int main(){
+    int age;
+
+    cout<<"enter your age: ";
+    cin>>age;
+
+    if(age>=18){
+        cout<<"you are eligible for voting";
+    }else{
+        cout<<"you are not eligible for voting";
+    }
+}
+
+q) write a program to print the ASCII value of a character.
+
+#include<iostream>
+using namespace std;
+int main(){
+    char ch;
+
+    cout<<"enter  the character: ";
+    cin>>ch;
+    cout<<"the ASCII value of "<<ch<<" is: "<<int(ch);
+}
+
+q)write a program to print the leargest value in two value.
+
+
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+    int b;
+
+    cout<<"enter the number of a: ";
+    cin>>a;
+
+    cout<<"enter the number of b: ";
+    cin>>b;
+
+    if(a>b){
+        cout<<"a is a leargest valued "<<endl;
+    }else{
+        cout<<"b is a leargest valued "<<endl;
+    }
+}
+
+q)calculate the triangle area ?
+
+#include<iostream>
+using namespace std;
+int main(){
+    int height;
+    int breath;
+    int area;
+
+
+    cout<<"enter the number of height: ";
+    cin>>height;
+
+    cout<<"enter the number of breath: ";
+    cin>>breath;
+
+    area= 0.5*height*breath;
+
+    cout<<"the triabgle area is: "<<area<<endl;
+
+}
+
+q) Write a program to find power of any number x ^ y.
+
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main()
+{
+  int x;
+  int y;
+  int c;
+
+  cout<<"enter the number of x:";
+  cin>>x;
+
+  cout<<"enter the number of y: ";
+  cin>>y;
+
+  c=(pow (x ,y));
+
+  cout<<" x power is : "<<c<<endl;
+  return 0;
+}
+
+q)Write a Program that takes minutes as input and display the total number of hours and minutes.
+
+#include<iostream>
+using namespace std;
+int main(){
+ int menits;
+ int total;
+ int total1;
+ cout<<"enter the menits: ";
+ cin>>menits;
+
+  total =menits/60;
+  total1 = menits%60;
+  cout<<"hours is : "<<total<<endl;
+  cout << "menits is : " << total1 << endl;
+}
+
+q)Write a program to compute the perimeter and area of a circle with a given radius.
+
+#include <iostream>
+using namespace std;
+int  main()
+{
+    int radius;
+    float area, perimeter;
+    radius = 6;
+    perimeter = 2 * 3.14 * radius;
+     cout<<("Perimeter of the Circle = %0.2f inches\n", perimeter);
+    area = 3.14 * radius * radius;
+    cout<<("Area of the Circle = %0.2f square inches\n", area);
+}
+
+q)Write a program to print your name, date of birth. and mobile number
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "ranajit bera wellcome to our new dsa jurney ";
+    cout << "\n29 septembar 2005";
+    cout << "\nPhone Number : +919609794466";
+    return 0;
+}
+
+Q)2. Write a program to enter two numbers and perform all arithmetic operations.
+
+
+#include <iostream>
+    using namespace std;
+int main()
+{
+    int x, y, sum, sub, mul;
+    float div;
+    printf("Enter Two Numbers = ");
+    scanf("%d %d", &x, &y);
+    sum = x + y;
+    sub = x - y;
+    mul = x * y;
+    div = x / y;
+
+    cout << endl
+         << "Summation of Two Numbers = " << sum << endl;
+    cout << "Subtraction of Two Numbers = " << sub << endl;
+    cout << "Multiplication of Two Numbers = " << mul << endl;
+    cout << "Division of Two Numbers = " << div << endl;
+
+    return 0;
+}
+
+
+//this is code area of the circle ?
+#include <iostream>
+using namespace std;
+int main()
+{
+    int radius = 6;
+    float area, perimeter = 2 * 3.14 * radius;
+    printf("Perimeter of the Circle = %0.2f inches\n", perimeter);
+    area = 3.14 * radius * radius;
+    printf("Area of the Circle = %0.2f square inches\n", area);
+    return 0;
+}
+*/
+// 7.	Write a program that converts Centigrade to Fahrenheit.
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float f, c;
+    cout << "Enter Celsius temperature: ";
+    cin >> f;
+
+    f = ((9 * c) / 5) + 32;
+
+    cout << "Fahrenheit: " << f;
+
+    return 0;
+}
---
+++ firstclass_basicc++/basicclass/basic1p.cpp
@@ -0,0 +1,743 @@
+
+/*this is problem statement are basic or operators problem
+q) write a program to print your name,date of birth and mobile number
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int phone_number;
+    string name;
+    int dob;
+
+    cout<<"my phone_number is : 9609794466"<<endl;
+    cout<<"my name is : Ranajit bera"<<endl;
+    cout<<"my date of birth is : 29/09/2005"<<endl;
+    return 0;
+}
+
+q) write a progrma to enter length and breadth of a rectangle and finish its perimeter
+firstly you must be know that rectangle perimeter law 𝑃=2×(𝑙+𝑤)
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int l;
+    int p;
+    int w;
+
+    cin>>l;
+    cin>>w;
+    p = 2*(l+w);
+
+    cout<<"A rectangle perimeter is:" <<p <<endl;
+}
+
+q)write a progrma to enter length and breadth of a rectangle and finish its area.
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a;
+    int l;
+    int w;
+
+    cin>>l;
+    cin>>w;
+    a=(l*w);
+    cout<<"A rectangle area is : "<<a<<endl;
+}
+
+Q) Write a Program to calculate area of an equilateral triangle.
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a;
+    int b;
+    int c;
+    int d;
+
+
+    cout<<"enter the value of a : "<<endl;
+    cin>>a;
+    cout<<"enter the value of b: "<<endl;
+    cin>>b;
+    cout<<"enter the value of c"<<endl;
+    cin>>c;
+    d = a+b+c;
+    cout<<"your equilateral triangle is : "<<d<<endl;
+}
+
+Q)    Write a program to compute the perimeter and area of a circle with a given radius
+
+FIRSTly this problem solve you must be know that formula of circle  Perimeter (Circumference): \(C = 2 \pi r\)    Area: \(A = \pi r^2\)
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int c;
+    float a;
+    int r;
+    double const pi=3.14;
+    cin>>c;
+    cin>>r;
+    a= pi*r*r;
+    c=2*pi*r;
+    cout<<"circle perimeter is : "<<c<<endl;
+    cout<<"circle area is : "<<a<<endl;
+
+}
+
+Q)Write a program to convert specified days into years, weeks, and days.
+
+#include<bits/stdc++.h>
+using namespace std;
+int main (){
+    int days;
+    int year;
+    int weeks;
+
+    cout<<"enter the days: "<<endl;
+    cin>>days;
+
+    year=days/365;
+    weeks=days/7;
+
+    cout<<"year and weeks is : "<<year<<endl;
+    cout<<"weeks is : "<<weeks<<endl;
+
+}
+
+Q) abcd
+   abcd
+   abcd
+   abcd
+
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int i;
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cout<<"ABCD";
+            break;                 //i besically use this break veriable for duplicate line no print
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+
+1234
+1234
+1234
+1234
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int i;
+    int n;
+    cout<<"enter the number of line : "<<endl;
+    cin>>n;
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n+1;j++){
+            cout<<"abcd";
+            break;
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
+
+*
+**
+***
+****
+*****
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter the number of line : ";
+    cin>>n;
+
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
+
+this is same question using while loop just try .
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter the number of line: ";
+    cin>>n;
+    int i=1;
+
+    while(i<=n){
+        cout<<"123";
+        i++;
+        cout << endl;
+    }
+
+
+    return 0;
+}
+q) function use how to use
+
+#include<bits/stdc++.h>
+using namespace std;
+void mininum(int a, int b)
+{
+    if (a < b)
+    {
+        cout << "this number is minimum :" << a << endl;
+    }
+    else
+    {
+        cout << "this number is not minimum , it's a maximum number :" << b << endl;
+    }
+}
+int main()
+{
+    int a, b;
+    cout << "enter two number: ";
+    cin >> a >> b;
+    mininum(7, 5);
+
+    return 0;
+}
+
+44. Write a program to print the day of the week name using a switch case.
+
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int day;
+    cout << "Enter the day number (1-7): ";
+    cin >> day;
+
+    switch (day)
+    {
+    case 1:
+        cout << "Monday" << endl;
+        break;
+    case 2:
+        cout << "Tuesday" << endl;
+        break;
+    case 3:
+        cout << "Wednesday" << endl;
+        break;
+    case 4:
+        cout << "Thursday" << endl;
+        break;
+    case 5:
+        cout << "Friday" << endl;
+        break;
+    case 6:
+        cout << "Saturday" << endl;
+        break;
+    case 7:
+        cout << "Sunday" << endl;
+        break;
+    default:
+        cout << "Invalid day number" << endl;
+    }
+
+    return 0;
+}
+
+q) this is function use
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void ranajit(){
+    cout<<"hello ranajit bera\n";
+}
+int main(){
+    ranajit();
+    ranajit();
+    return 0;
+}
+
+Q)TWO SUM USING FUNCTION
+
+#include<bits/stdc++.h>
+using namespace std;
+void ranajit(){
+    int a , b , sum;
+    float multiple;
+    cout<<"enter the number: ";
+    cin>>a>>b;
+
+    sum=a+b;
+    multiple=a*b;
+
+    cout<<"output"<<sum<<endl;
+    cout << "output" << multiple<< endl;
+}
+int main(){
+    ranajit();
+    return 0;
+}
+
+
+q) just using name with integer value try ...
+
+#include<bits/stdc++.h>
+using namespace std;
+int hello(){
+     cout<<"hello ranajit bera! "<<endl;
+
+    return 45;
+}
+int main(){
+    cout<<hello();
+}
+
+
+******************************************for loop question **********************************************
+// 53.	Write a program to Print your name 20 times using a for loop, while loop and do while loop.
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        cout<<" ranajit bera"<<endl;
+    }
+    return 0;
+}
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int i=1;
+    int n;
+    cin>>n;
+    while(i<=n){
+        cout<<"ranajit bera"<<endl;
+        i++;
+    }
+
+    return 0;
+}
+
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int i=1;
+    int n;
+    cin>>n;
+    do{
+     cout<<"ranajit bera "<<endl;
+     i++;
+    }while(i<=n);
+    return 0;
+}
+
+55.	Write a program to Calculate the Sum of 1 to 10 numbers using a for loop, while loop and do while loop.
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int i=1;
+    int n=10;
+    int sum=0;
+    for(int i=1;i<=n;i++){
+     sum=sum+i;
+
+     cout<<"sum is: "<<sum<<endl;
+    }
+
+    return 0;
+}
+// 5. Write a program to calculate the perimeter and area of a circle with a given radius.
+#include <iostream>
+    using namespace std;
+int main()
+{
+    int radius;
+    float area, perimeter;
+    radius = 6;
+
+    perimeter = (2 * 3.1416 * radius);
+    area = (3.1416 * radius * radius);
+
+    cout << "Perimeter of the Circle = " << perimeter << " inches\n";
+    cout << "Area of the Circle = " << area << " square inches\n";
+
+    return 0;
+}
+
+Q)224.	Write a program to find a cube of any number using a user-defined function.
+
+#include<bits/stdc++.h>
+using namespace std;
+void cube(){
+    int x;
+    int n;
+    cin>>n;
+    x=n*n*n;
+
+}
+int main(){
+    cube();
+    int x;
+    cout<<"cube is : "<<x<<endl;
+}
+
+Function to calculate the cube of a number
+double cube(double num)
+{
+    return num * num * num;
+}
+
+int main()
+{
+    double number;
+
+    cout << "Enter a number: ";
+    cin >> number;
+
+    double result = cube(number);
+
+    cout << "Cube of " << number << " is " << result << endl;
+
+    return 0;
+}
+
+
+//Write a program to print a matrix using array.
+#include <bits/stdc++.h>
+using namespace std;
+    int main()
+{
+    int num[3][3], i, j;
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            cout << j + 1 << "\t";
+        }
+        cout << endl;
+    }
+    return 0;
+}
+
+
+//7. Write a program that converts Centigrade to Fahrenheit.
+#include <iostream>
+    using namespace std;
+
+int main()
+{
+    float f, c;
+    cout << "Enter Celsius temperature: ";
+    cin >> c;
+
+    f = ((9 * c) / 5) + 32;
+
+    cout << "Fahrenheit: " << f;
+
+    return 0;
+}
+
+
+
+
+
+
+
+//1.	Write a program to enter any number and calculate its square root.
+#include <iostream>
+#include<math.h>
+using namespace std;
+int main(){
+    int num, result;
+    cout<<"Enter a number: ";
+    cin>>num;
+
+    result = sqrt(num);
+
+    cout<<"Square root = "<<result;
+    return 0;
+}
+
+//93. Write a program to read and print elements of an array.
+#include <iostream>
+    using namespace std;
+
+int main()
+{
+    int arr[5];
+    cout << "Enter 5 elements of the array: ";
+    for (int i = 0; i < 5; i++)
+    {
+        cin >> arr[i];
+    }
+    cout << "The elements of the array are: ";
+    for (int i = 0; i < 5; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
+//23. Write a program to check whether a number is negative or positive or zero.
+
+#include<iostream>
+using namespace std;
+int main(){
+    int num;
+    cout<<"Enter a Number: ";
+    cin>>num;
+
+    if(num < 0){
+        cout<<num<<" is negative number.";
+    }
+    else if(num == 0){
+        cout<<num<<" is zero.";
+    }
+    else{
+        cout<<num<<" is positive number.";
+    }
+
+    return 0;
+}
+
+
+
+//Write a program to find the power of any number x ^ y.
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int x;
+    int y=2;
+    int number;
+
+    cout<<"enter the number of x: ";
+    cin>>x;
+
+
+    number=pow(x,y);
+
+    cout<<"power number is : "<<number<<endl;
+
+    return 0;
+}
+
+
+// 2 + 4 + 6 + 8 + 10 + 12 + 14 + ………………………… + 26 = ?
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int i;
+
+    for(int i=2;i<=26;i+=2){
+        cout<<i<<endl;
+    }
+
+    return 0;
+}
+
+// 2 + 4 + 6 + 8 + 10 + 12 + 14 = ?
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int i;
+
+    for(int i=0;i<=14;i+=2){
+        cout<<i<<endl;
+    }
+
+    return 0;
+}
+
+// 1 +11 + 111 + 1111 + .. n terms
+
+#include<bits/stdc++.h>
+using namespace std;
+
+    int main()
+    {
+        int n;
+        long long term = 0, sum = 0;
+
+        printf("Enter number of terms: ");
+        scanf("%d", &n);
+
+        for (int i = 1; i <= n; i++)
+        {
+            term = term * 10 + 1;
+            sum += term;
+        }
+
+        printf("Sum = %lld\n", sum);
+
+        return 0;
+    }
+
+//99.Write a Program for sum of 5 numbers using Array.
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n = 5;
+    int ranajit[]={2,2,2,1,1};
+    int sum=0;
+    for(int i=0; i<n; i++){
+        sum += ranajit[i];
+    }
+    cout << "total array sum is : " << sum << endl;
+    return 0;
+}
+
+//.Write a program to read the age of 10 persons and count the number of persons in the age
+// group 50 to 60.
+#include <stdio.h>
+    void main()
+{
+    int t, age, i;
+    t = 0;
+    for (i = 1; i <= 10; i++)
+    {
+        printf("Enter %d no person's age = ", i);
+        scanf("%d", &age);
+        if (age >= 50 && age <= 60)
+        {
+            t++;
+        }
+    }
+    printf("Total persons between 50 to 60 ages = %d", t);
+}
+
+
+q)Write a program to add two numbers (Input collect from user) using user defined function.
+
+#include<bits/stdc++.h>
+using namespace std;
+void add(){
+    int a;
+    int b;
+    int add;
+    cout<<"enter the number of a:";
+    cin>>a;
+    cout<<"enter the number of b:";
+    cin>>b;
+
+    add=a+b;
+    cout<<"The sum is: "<<add<<endl;
+}
+int main(){
+    add();
+    return 0;
+}
+
+//144.Write a program to find maximum and minimum between two numbers.
+
+#include<bits/stdc++.h>
+using namespace std;
+void max_min(int a, int b){
+    if(a>b){
+        cout<<"this number is maximum : "<<a<<endl;
+    }
+    else{
+        cout<<"this number is minimum : "<<b<<endl;
+    }
+
+}
+int main(){
+    int a,b;
+    cout<<"enter the number of a: ";
+    cin>>a;
+    cout<<"enter the number of b: ";
+    cin>>b;
+    max_min(a,b);
+}
+
+//write a program to multiple table
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int num;
+    cout<<"enter the number of table: ";
+    cin>>num;
+    for(int i=1;i<=10;i++){
+        cout<<num<<"*"<<i<<"="<<num*i <<endl;
+    }
+    return 0;
+
+}
+
+//34.	Write a program to input the angles of a triangle and check whether a triangle is valid or not.
+#include <iostream>
+using namespace std;
+
+int main() {
+    int angle1, angle2, angle3, sum;
+
+    cout << "Enter three angles of a triangle: ";
+    cin >> angle1 >> angle2 >> angle3;
+
+    sum = angle1 + angle2 + angle3;
+
+    if (sum == 180) {
+        cout << "The triangle is valid.";
+    }
+    else {
+        cout << "The triangle is not valid.";
+    }
+
+    return 0;
+}
+
+//57. Write a program to print even numbers up to N.
+#include <iostream>
+    using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter a positive integer: ";
+    cin >> n;
+
+    cout << "Even numbers up to " << n << " are: ";
+    for (int i = 2; i <= n; i += 2)
+    {
+        cout << i << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
+*/
+//ascii value
+#include<iostream>
+using namespace std;
+int main(){
+    char ch;
+    cout<<"enter the char or symbol: ";
+    cin>>ch;
+    cout<<"ASCII value is :"<<int(ch);
+}
