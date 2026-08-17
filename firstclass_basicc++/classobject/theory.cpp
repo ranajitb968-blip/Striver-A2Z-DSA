086df0ebaa01562d74cd9a560fd74be34fbe06ab
ranajitb968-blip <ranajitb968@gmail.com>
2026-08-17 20:46:48 +0530

    new class and object problem solve on theory based

---
+++ firstclass_basicc++/classobject/theory.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+class Car
+{
+public:
+    // Attribute
+    string color;
+    string model;
+
+    // Method
+    void startEngine()
+    {
+        cout << "Engine started" << endl;
+    }
+};
