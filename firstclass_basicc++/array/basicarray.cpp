c2ac56c296746638689ce92b2e354233773dbcc3
ranajitb968-blip <ranajitb968@gmail.com>
2026-08-19 21:15:29 +0530

    sovle take input array number form users:

--- firstclass_basicc++/array/basicarray.cpp
+++ firstclass_basicc++/array/basicarray.cpp
@@ -1,3 +1,5 @@
+/*
+
 // 99.Write a Program for sum of 5 numbers using Array
 #include<bits/stdc++.h>
 using namespace std;
@@ -10,5 +12,27 @@ int main(){
        cout<<"total sum is: "<<ans<<endl;
     }
 
+    return 0;
+}
+
+*/
+
+// 99.Write a Program for sum of 5 numbers using user input array
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int arr[5];
+    int result=0;
+    
+   for(int i=0;i<5;i++){
+    cout<<"enter the number: ";
+    cin>>arr[i];
+   }
+
+ for(int i=0;i<5;i++){
+    result=result+arr[i];
+    cout<<"total array sum is : "<<result<<endl;
+ }
     return 0;
 }
