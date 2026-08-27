508130e24307d9253f485ff2984425ee377c817b
ranajitb968-blip <ranajitb968@gmail.com>
2026-08-27 23:45:03 +0530

    new basic problem solve

--- firstclass_basicc++/basicclass/basic1p.cpp
+++ firstclass_basicc++/basicclass/basic1p.cpp
@@ -791,6 +791,45 @@ int main()
     }
     return 0;
 }
-
-
 */
+
+// 43.	Write a program to input electricity unit charges and calculate the total electricity bill according to the given condition:
+// For the first 50 units, Rs. 0.50/unit
+// For the next 100 units Rs. 0.75/unit
+// For the next 100 units Rs. 1.20/unit
+// For units above 250 Rs. 1.50/unit
+// An additional surcharge of 20% is added to the bill.
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int units;
+    float total_bill, surcharge = 0.2;
+
+    cout << "Enter electricity units consumed: ";
+    cin >> units;
+
+    if(units <= 50) {
+        total_bill = units * 0.5;
+    }
+    else if(units <= 150) {
+        total_bill = 25 + (units - 50) * 0.75;
+    }
+    else if(units <= 250) {
+        total_bill = 100 + (units - 150) * 1.20;
+    }
+    else {
+        total_bill = 220 + (units - 250) * 1.50;
+    }
+
+    total_bill += total_bill * surcharge;
+
+    cout << "Electricity Bill = Rs. " << total_bill;
+
+    return 0;
+}
+
+
