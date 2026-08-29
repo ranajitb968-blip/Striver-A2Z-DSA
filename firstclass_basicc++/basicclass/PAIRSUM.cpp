8dc9abf8b6bc0e63934fd0c2f7a7b27006099c28
ranajitb968-blip <ranajitb968@gmail.com>
2026-08-29 21:54:13 +0530

    optimized code with using two pointer approch

--- firstclass_basicc++/basicclass/PAIRSUM.cpp
+++ firstclass_basicc++/basicclass/PAIRSUM.cpp
@@ -1,3 +1,4 @@
+/*
 #include<iostream>
 #include<vector>
 using namespace std;
@@ -27,3 +28,45 @@ int main(){
 
 
 // time complexity is O(n^2) and space complexity is O(1)
+
+
+
+** next optimized code is below using two pointer
+*/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> pairSum(vector<int> nums, int target)
+{
+    vector<int> ans;
+    int n = nums.size();
+    int left = 0; int right = n - 1;
+
+    while(left< right ){
+        int pairSum = nums[left] + nums[right];
+        if(pairSum > target){
+            right--;
+        }
+        else if(pairSum < target){
+            left++;
+        }
+        else{
+            ans.push_back(left);
+            ans.push_back(right);
+            return ans;
+        }
+    }
+  return ans;
+
+}
+int main()
+{
+    vector<int> nums = {2, 7, 11, 15};
+    int target = 17;
+
+    vector<int> ans = pairSum(nums, target);
+    cout << ans[0] << " " << ans[1] << endl;
+    return 0;
+}
